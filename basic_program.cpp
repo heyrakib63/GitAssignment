fc1febe308488a51c139d0940e45d9f3eabc101e
Md. Rakib Hossain <md.rakib@brainstation-23.com>
2025-04-09 12:25:18 +0600

    Revert "Add factorial program"
    This reverts commit 0af200f7a806404a1ce535eb89115b878dc8f9d2.

--- basic_program.cpp
+++ basic_program.cpp
@@ -4,13 +4,7 @@ using namespace std;
 
 int main(){
 
-    int i, fact = 1, n;
-    cout << "Enter the value of n: ";
-    cin>>n;
-    for(i = 1; i<=n; i++){
-        fact*= i;
-    }
-    cout << "The factorial is: " << fact << endl;
+    cout << "Hello World" << endl;
 
     return 0;
 }
